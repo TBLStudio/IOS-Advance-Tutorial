0e8670b8d6d64a4f0e95c4deca1d7b8313cfa537
ngohongthai <ngohongthai.uet@gmail.com>
2016-05-01 00:07:43 +0700

    Section 30
    Add New Group button

--- WhaleTalk/WhaleTalk/AllChatsViewController.swift
+++ WhaleTalk/WhaleTalk/AllChatsViewController.swift
@@ -30,6 +30,7 @@ class AllChatsViewController: UIViewController, TableViewFetchedResultsDisplayer
         
         tableView.registerClass(ChatCell.self, forCellReuseIdentifier: cellIdentifier)
         tableView.tableFooterView = UIView(frame: CGRectZero)
+        tableView.tableHeaderView = createHeader()
         tableView.dataSource = self
         tableView.delegate = self
         
@@ -100,6 +101,47 @@ class AllChatsViewController: UIViewController, TableViewFetchedResultsDisplayer
         vc.context = context
         vc.chat = chat
         navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    private func createHeader () -> UIView {
+        let header = UIView()
+        let newGroupButton = UIButton()
+        newGroupButton.translatesAutoresizingMaskIntoConstraints = false
+        header.addSubview(newGroupButton)
+        
+        newGroupButton.setTitle("New Group", forState: .Normal)
+        newGroupButton.setTitleColor(view.tintColor, forState: .Normal)
+        newGroupButton.addTarget(self, action: #selector(AllChatsViewController.pressedNewGroup), forControlEvents: UIControlEvents.TouchUpInside)
+        
+        let border = UIView()
+        border.translatesAutoresizingMaskIntoConstraints = false
+        header.addSubview(border)
+        border.backgroundColor = UIColor.lightGrayColor()
+        
+        let constraints: [NSLayoutConstraint] = [
+            newGroupButton.heightAnchor.constraintEqualToAnchor(header.heightAnchor),
+            newGroupButton.trailingAnchor.constraintEqualToAnchor(header.layoutMarginsGuide.trailingAnchor),
+            border.heightAnchor.constraintEqualToConstant(1),
+            border.leadingAnchor.constraintEqualToAnchor(header.leadingAnchor),
+            border.trailingAnchor.constraintEqualToAnchor(header.trailingAnchor),
+            border.bottomAnchor.constraintEqualToAnchor(header.bottomAnchor)
+        ]
+        
+        NSLayoutConstraint.activateConstraints(constraints)
+        header.setNeedsLayout()
+        header.layoutIfNeeded()
+        
+        let height = header.systemLayoutSizeFittingSize(UILayoutFittingCompressedSize).height
+        var frame = header.frame
+        frame.size.height = height
+        header.frame = frame
+        
+        return header
+        
+    }
+    
+    func pressedNewGroup () {
+    
     }
     
 }
