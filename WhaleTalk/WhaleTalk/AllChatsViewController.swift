cc7a67c92f753d02ae0b7918563ee3562119f063
ngohongthai <ngohongthai.uet@gmail.com>
2016-04-30 16:21:36 +0700

    Section 22
    Create Temporary Child Context for Chats

--- WhaleTalk/WhaleTalk/AllChatsViewController.swift
+++ WhaleTalk/WhaleTalk/AllChatsViewController.swift
@@ -62,7 +62,9 @@ class AllChatsViewController: UIViewController, TableViewFetchedResultsDisplayer
     func newChat ()
     {
         let vc = NewChatViewController()
-        vc.context = context
+        let chatContext = NSManagedObjectContext(concurrencyType: NSManagedObjectContextConcurrencyType.MainQueueConcurrencyType)
+        chatContext.parentContext = context
+        
         vc.chatCreationDelegate = self
         let navVC = UINavigationController(rootViewController: vc)
         presentViewController(navVC, animated: true, completion: nil)
