323362b427583efc3987b20af813d37ad4a2f0f6
ngohongthai <ngohongthai.uet@gmail.com>
2016-05-01 11:23:11 +0700

    Section 32
    New Group UI

--- WhaleTalk/WhaleTalk/NewGroupViewController.swift
+++ WhaleTalk/WhaleTalk/NewGroupViewController.swift
@@ -15,15 +15,113 @@ class NewGroupViewController: UIViewController {
     var context: NSManagedObjectContext?
     var chatCreationDelegate: ChatCreationDelegate?
     
+    private let subjectField = UITextField()
+    private let characterNumberLabel = UILabel()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.backgroundColor = UIColor.whiteColor()
+        
+        //Setup Navigation bar
         title = "New Group"
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .Plain, target: self, action: #selector(NewGroupViewController.cancel))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Next", style: .Plain, target: self, action: #selector(NewGroupViewController.next))
+        updateNextButton(forCharCount: 0)
+        
+        //Setup Subject field
+        subjectField.placeholder = "Group Subject"
+        subjectField.delegate = self
+        subjectField.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(subjectField)
+        updateCharacterLabel(forCharCount: 0)
+        
+        //Setup Character number label
+        characterNumberLabel.textColor = UIColor.grayColor()
+        characterNumberLabel.translatesAutoresizingMaskIntoConstraints = false
+        subjectField.addSubview(characterNumberLabel)
+        
+        //Setup bottom border
+        let bottomBorder = UIView()
+        bottomBorder.backgroundColor = UIColor.lightGrayColor()
+        bottomBorder.translatesAutoresizingMaskIntoConstraints = false
+        subjectField.addSubview(bottomBorder)
+        
+        //Constraint for each element
+        let constraints: [NSLayoutConstraint] = [
+            subjectField.topAnchor.constraintEqualToAnchor(topLayoutGuide.bottomAnchor, constant: 20),
+            subjectField.leadingAnchor.constraintEqualToAnchor(view.layoutMarginsGuide.leadingAnchor),
+            subjectField.trailingAnchor.constraintEqualToAnchor(view.trailingAnchor),
+            
+            bottomBorder.widthAnchor.constraintEqualToAnchor(subjectField.widthAnchor),
+            bottomBorder.bottomAnchor.constraintEqualToAnchor(subjectField.bottomAnchor),
+            bottomBorder.leadingAnchor.constraintEqualToAnchor(subjectField.leadingAnchor),
+            bottomBorder.heightAnchor.constraintEqualToConstant(1),
+            
+            characterNumberLabel.centerYAnchor.constraintEqualToAnchor(subjectField.centerYAnchor),
+            characterNumberLabel.trailingAnchor.constraintEqualToAnchor(subjectField.layoutMarginsGuide.trailingAnchor)
+        ]
+        
+        NSLayoutConstraint.activateConstraints(constraints)
+        
+        
         
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    
+    func cancel () {
+        dismissViewControllerAnimated(true, completion: nil)
+    
+    }
+    
+    func next () {
+    
+    }
+    
+    func updateCharacterLabel (forCharCount length: Int) {
+        characterNumberLabel.text = String (25 - length)
+    }
+    
+    func updateNextButton (forCharCount length: Int) {
+        if length == 0 {
+            navigationItem.rightBarButtonItem?.tintColor = UIColor.lightGrayColor()
+            navigationItem.rightBarButtonItem?.enabled = false
+        } else {
+            navigationItem.rightBarButtonItem?.tintColor = view.tintColor
+            navigationItem.rightBarButtonItem?.enabled = true
+        }
+    }
+    
 
 }
+
+extension NewGroupViewController: UITextFieldDelegate {
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        let currentCharacterCount = textField.text?.characters.count ?? 0
+        let newLength = currentCharacterCount + string.characters.count - range.length
+        
+        if newLength <= 25 {
+            updateCharacterLabel(forCharCount: newLength)
+            updateNextButton(forCharCount: newLength)
+            return true
+        }
+        return false
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
